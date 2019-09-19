d46bfa06a0e7af99490dc61575a23b72a5687daa
Mohit <mohitaprilsaini@gmail.com>
2019-09-18 22:37:40 -0400

    catgory repository created

---
+++ src/main/java/com/example/Expense/tracker/Repository/CategoryRepository.java
@@ -0,0 +1,12 @@
+package com.example.Expense.tracker.Repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.Expense.tracker.Model.Category;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+	 
+	Category findByName(String name);
+	 
+
+}
