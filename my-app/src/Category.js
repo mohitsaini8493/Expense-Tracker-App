7c30b00150118f4f6b5b7200aa41b960b7e541d2
Mohit <mohitaprilsaini@gmail.com>
2019-09-23 18:55:24 -0400

    category.js file added to the project and connected to spring project

---
+++ my-app/src/Category.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react'
+import { awaitExpression } from '@babel/types';
+
+export default class Category extends Component {
+    state = {
+        isLoading : true,
+        Categories : []
+    }
+    async componentDidMount() {
+        const response  = await fetch("api/category");
+        const body = await response.json();
+        this.setState({Categories : body, isLoading : false});
+    }
+    render() {
+        const{Categories, isLoading} = this.state ;
+        if(isLoading){
+            return (<div>Loading ..</div>
+                );
+        }
+        return (
+            <div>
+                <h2>Categories</h2>
+                {
+                    Categories.map(category =>
+                        <div id={category.id}>
+                            {category.name}
+                        </div>)
+                }
+            </div>
+        );
+    }
+}
